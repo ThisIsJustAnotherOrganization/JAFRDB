054671b4e4f2a1dbbad7e7032882cc4e0c3716bc
beepbeat <zfiremani885@gmail.com>
2017-08-12 21:46:50 +0200

    Fixed errors in Code occurring due to me making a shitty gradle file/something weird happening. Fixed RegEx String being not escaped (''\' still needs to be escaped when casting a String to a RegEx.

--- src/LogHandler.kt
+++ src/LogHandler.kt
@@ -17,7 +17,7 @@ var tailerStopped = false
 
 
 class listener : TailerListenerAdapter(){
-    val ratsigRegex = "RATSIGNAL.*CMDR\\s\\02?(.*?)\\02?\\s\\-.*System\\72\\s\\02?(.*?)(?:\\s[Ss][Yy][Ss][Tt][Ee][Mm])?\\02?\\s\\(.*Platform\\72\\s\\02?(?:\\03\\d\\d)?(\\w+).*O2\\72\\s\\02?(?:\\03\\d\\d)?((?:NOT\\s)?OK).*Language\\72\\s.*\\((..).*\\(Case\\s#(\\d*)\\)".toRegex()
+    val ratsigRegex = "DRILLSIGNAL.*CMDR\\s\\02?(.*?)\\02?\\s-.*System\\x3a\\s\\02?(.*?)(?:\\s[Ss][Yy][Ss][Tt][Ee][Mm])?\\02?\\s\\(.*Platform\\72\\s\\02?(?:\\03\\d\\d)?(\\w+).*O2\\72\\s\\02?(?:\\03\\d\\d)?((?:NOT\\s)?OK).*Language\\72\\s.*\\((..).*\\(Case\\s#(\\d*)\\)".toRegex()
     //regex at https://regex101.com/r/Vjtkxk/4
     
     override fun init(tailer: Tailer?) {
@@ -49,7 +49,7 @@ class listener : TailerListenerAdapter(){
     fun mirc(l : String?){
         var line : String = l!!.replace("\t", " ")
         val nick = line.split(Pattern.compile(" "), 3)[1].replace("<", "").replace(">", "").replace("+", "").replace("%", "").replace("@", "").replace("~", "").replace("&", "") // strip: +%@~&
-        line = line.split(Pattern.compile(" "), 3)[2].trim()
+        line = line.split(Pattern.compile(" "), 3)[2].trim()//.replace("[\\x02\\x1F\\x0F\\x16]|\\x03(\\d\\d?(,\\d\\d?)?)?".toRegex(), "")
         handleMessage(nick, line)
     }
 
@@ -165,15 +165,23 @@ class listener : TailerListenerAdapter(){
             }
         }
         else {
-            if (message.startsWith("RATSIGNAL")){
+            if (message.startsWith("DRILLSIGNAL")){
                 //RATSIGNAL - CMDR killcrazycarl - System: COL 285 sector GM-V D2-110 (225.32 LY from Sothis) - Platform: XB - O2: OK - Language: English (en-US) (Case #1)
-                val matches : MatchGroupCollection = ratsigRegex.matchEntire(message)?.groups!!
-                val name : String = matches.get(1)?.value!!
-                val system : String = matches.get(2)?.value!!
-                val platform : String = matches.get(3)?.value!!
-                val cr : Boolean = matches.get(4)?.value!! != "OK"
-                val lang : String = matches.get(5)?.value!!
-                val number : Int = matches.get(6)?.value!!.toInt()
+/*                val matches : MatchGroupCollection = ratsigRegex.matchEntire(message)?.groups!!
+                val name : String = matches.get(1)?.value ?: ""
+                val system : String = matches.get(2)?.value ?: ""
+                val platform : String = matches.get(3)?.value ?: ""
+                val cr : Boolean = matches.get(4)?.value != "OK"
+                val lang : String = matches.get(5)?.value ?: ""
+                val number : Int = matches.get(6)?.value?.toInt() ?: -1*/
+
+                val parts = message.split(" - ")
+                val name : String = parts[1].replace("CMDR ", "")
+                val system : String = parts[2].replace("System: ", "").split("(")[0]
+                val platform : String = parts[3].replace("Platform: ", "")
+                val cr : Boolean = parts[4].replace("O2: ", "") != "OK"
+                val lang : String = parts[5].replace("Language: ", "").split(" ")[1].replace("(", "").replace(")", "").split("-")[0]
+                val number : Int = parts.last().split(" ").last().replace("(", "").replace(")", "").replace("#", "").toInt()
 
                 rescues.add(Rescue(name, System(system), lang, number, platform, cr))
 
@@ -214,7 +222,7 @@ class listener : TailerListenerAdapter(){
                 }
 
                 if (message.contains("clear")){
-                    getCase(message, nick).rats.filter{it.name == nick}.forEach { it.status.interdicted = TRUE }
+                    getCase(message, nick).rats.filter{it.name == nick}.forEach { it.status.interdicted = FALSE }
                 }
 
                 //TODO implement Disconnected
